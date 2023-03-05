30c43c767079b9d29d48a0c56aa0b1ea2f2a92b5
Elise <eliseraney@gmail.com>
2023-03-05 11:45:40 -0700

    Home page updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home() {
       <section className="legend">
         <div>
           <img
-            src="/images/carrot-pink.png"
+            src="/images/carrot-dark-orange.png"
             alt="Purple carrot"
             className="carrot"
           />
